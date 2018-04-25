7527706450b149d5d42bffb98d785abb2602350f
Ethan McKenzie <mckenzieed@gmail.com>
2018-04-24 21:16:17 -0500

    Some project changes

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name='EIFT',
     version='0.0.1',
-    packages=[''],
+    packages=['EIFT-Core'],
     url='',
     license='',
     author='Ethan',
     author_email='mckenzieed@gmail.com',
-    description='News/user submitted content website'
+    description='News article and sources core repo to be used by other packages.'
 )
